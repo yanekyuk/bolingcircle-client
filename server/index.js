a9ef672a68ba915bb602c9fa59b57093f8638003
Yanek Yük <yanechednezzar@gmail.com>
2019-02-06 14:48:08 +0300

    Migrated to Express

--- server/index.js
+++ server/index.js
@@ -1,29 +1,35 @@
-'use strict'
-const path = require('path')
+const express = require('express')
 const consola = require('consola')
-const feathers = require('feathers')
-const configuration = require('feathers-configuration')
-const middleware = require('./middleware')
+const { Nuxt, Builder } = require('nuxt')
+const app = express()
 
-const app = feathers()
+// Import and Set Nuxt.js options
+const config = require('../nuxt.config.js')
+config.dev = !(process.env.NODE_ENV === 'production')
 
-app.configure(configuration(path.join(__dirname, './'))).configure(middleware)
+async function start() {
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config)
 
-const host = app.get('host')
-const port = app.get('port')
+  const {
+    host = process.env.HOST || '127.0.0.1',
+    port = process.env.PORT || 3000
+  } = nuxt.options.server
 
-process.on('nuxt:build:done', err => {
-  if (err) {
-    consola.error(err)
-    process.exit(1)
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
   }
-  const server = app.listen(port)
-  server.on('listening', () =>
-    consola.ready({
-      message: `Feathers application started on ${host}:${port}`,
-      badge: true
-    })
-  )
-})
 
-module.exports = app
+  // Give nuxt middleware to express
+  app.use(nuxt.render)
+
+  // Listen the server
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  })
+}
+start()
